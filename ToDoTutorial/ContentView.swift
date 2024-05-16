4e25957965a42804a4f6ab3d7774158151b925cd
Chelsea <chelsea@tripwiretech.com>
2024-05-16 12:59:40 +1200

    Initial Commit

---
+++ ToDoTutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoTutorial
+//
+//  Created by Charles Suddens on 16/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoTutorial/ToDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTutorialApp.swift
+//  ToDoTutorial
+//
+//  Created by Charles Suddens on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
